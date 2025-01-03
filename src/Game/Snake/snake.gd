6278f5e193345cf2cc9579a4d7bd7ff9916c4a58
SP4R0W <sp4r0wxxx@gmail.com>
2025-01-03 09:48:22 +0100

    Basic movement reworked

--- src/Game/Snake/snake.gd
+++ src/Game/Snake/snake.gd
@@ -1,11 +1,48 @@
 class_name Snake extends TileMapLayer
 
+@onready var move_timer: Timer = $MoveTimer
 
-# Called when the node enters the scene tree for the first time.
+var current_occupied: Array[Vector2i] = []
+var current_dir: Vector2i = Vector2i.RIGHT
+
 func _ready() -> void:
-	pass # Replace with function body.
+	current_occupied.append(get_used_cells()[0])
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("down"):
+		change_dir_manual(Vector2i.DOWN)
+	elif Input.is_action_just_pressed("up"):
+		change_dir_manual(Vector2i.UP)
+
+	if Input.is_action_just_pressed("left"):
+		change_dir_manual(Vector2i.LEFT)
+	elif Input.is_action_just_pressed("right"):
+		change_dir_manual(Vector2i.RIGHT)
+
+func _on_move_timer_timeout() -> void:
+	move(current_dir)
+
+func _move_single(direction: Vector2i) -> void:
+	var head: Vector2i = current_occupied[0]
+	var new_head: Vector2i = Vector2i(head.x + direction.x, head.y + direction.y)
+
+	if check_if_borders(new_head):
+		return
+
+	set_cell(new_head,2,Vector2i(0,0))
+	set_cell(head)
+	current_occupied[0] = new_head
+
+func move(direction: Vector2i) -> void:
+	if current_occupied.size() == 1:
+		_move_single(direction)
+		return
+
+func check_if_borders(coords: Vector2i) -> bool:
+	return coords.x > 19 or coords.y > 19
+
+func change_dir_manual(new_dir: Vector2i) -> void:
+	move_timer.stop()
+	current_dir = new_dir
+	move(current_dir)
+	move_timer.start()
--- src/Global/Global.gd
+++ src/Global/Global.gd
@@ -3,5 +3,5 @@ extends Node
 @onready var loading_transition: String = "res://src/FadeTransition/FadeTransition.tscn"
 
 const MOVEMENT_SIZE = 64
-const MAX_X = 23
-const MAX_Y = 13
+const MAX_X = 19
+const MAX_Y = 19
