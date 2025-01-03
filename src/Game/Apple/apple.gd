9577a644527f5527835d03568d7b25b7556fbcce
SP4R0W <sp4r0wxxx@gmail.com>
2025-01-03 10:47:40 +0100

    Movement system finished; added basic apple spawn functionality

--- src/Game/Apple/apple.gd
+++ src/Game/Apple/apple.gd
@@ -1,8 +1,41 @@
-extends Sprite2D
+class_name Apple extends TileMapLayer
+
+@export var max_normal: int = 1
+@export var max_golden: int = 0
+@export var max_rotten: int = 0
+
+var snake_occupied_cells: Array[Vector2i] = []
+
+func is_apple(cell: Vector2i) -> bool:
+	return get_cell_source_id(cell) >= 0
+
+func is_rotten(cell: Vector2i) -> bool:
+	return get_cell_source_id(cell) == 0
+
+func spawn_apple() -> void:
+	var coord: Vector2i = Vector2i(randi_range(0,Global.MAX_X), randi_range(0, Global.MAX_Y))
+
+	if coord in snake_occupied_cells or coord in get_used_cells():
+		while coord in snake_occupied_cells or coord in get_used_cells():
+			coord = Vector2i(randi_range(0,Global.MAX_X), randi_range(0, Global.MAX_Y))
+
+	set_cell(coord,1,Vector2i(0,0))
+
+func _process(delta: float) -> void:
+	print(snake_occupied_cells)
+
+func _on_normal_timer_timeout() -> void:
+	max_normal += 1
 
 
-func _ready() -> void:
-	position = Vector2(64 * randi_range(0, Global.MAX_X - 1),64 * randi_range(0, Global.MAX_Y - 1))
+func _on_golden_timer_timeout() -> void:
+	max_golden += 1
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	queue_free()
+
+func _on_rotten_timer_timeout() -> void:
+	max_rotten += 1
+
+
+func _on_snake_apple_hit(coord: Vector2i) -> void:
+	set_cell(coord)
+	spawn_apple()
--- src/Game/Snake/snake.gd
+++ src/Game/Snake/snake.gd
@@ -1,5 +1,13 @@
 class_name Snake extends TileMapLayer
 
+signal borders_hit()
+signal body_hit()
+
+signal apple_hit(coord: Vector2i)
+signal rotten_hit()
+
+@export var apple_tilemap: Apple
+
 @onready var move_timer: Timer = $MoveTimer
 
 var current_occupied: Array[Vector2i] = []
@@ -7,8 +15,10 @@ var current_dir: Vector2i = Vector2i.RIGHT
 
 func _ready() -> void:
 	current_occupied.append(get_used_cells()[0])
+	apple_tilemap.snake_occupied_cells = current_occupied
+	apple_tilemap.spawn_apple()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("down"):
 		change_dir_manual(Vector2i.DOWN)
 	elif Input.is_action_just_pressed("up"):
@@ -22,21 +32,26 @@ func _process(delta: float) -> void:
 func _on_move_timer_timeout() -> void:
 	move(current_dir)
 
-func _move_single(direction: Vector2i) -> void:
+func move(direction: Vector2i) -> void:
 	var head: Vector2i = current_occupied[0]
 	var new_head: Vector2i = Vector2i(head.x + direction.x, head.y + direction.y)
 
 	if check_if_borders(new_head):
+		borders_hit.emit()
 		return
 
-	set_cell(new_head,2,Vector2i(0,0))
-	set_cell(head)
-	current_occupied[0] = new_head
+	if apple_tilemap.is_apple(new_head):
+		if apple_tilemap.is_rotten(new_head):
+			rotten_hit.emit()
+			return
 
-func move(direction: Vector2i) -> void:
-	if current_occupied.size() == 1:
-		_move_single(direction)
+		apple_hit.emit(new_head)
+		add_body(new_head)
 		return
+
+	set_cell(new_head,0,Vector2i(0,0))
+	current_occupied.insert(0,new_head)
+	set_cell(current_occupied.pop_back())
 
 func check_if_borders(coords: Vector2i) -> bool:
 	return coords.x > 19 or coords.y > 19
@@ -46,3 +61,7 @@ func change_dir_manual(new_dir: Vector2i) -> void:
 	current_dir = new_dir
 	move(current_dir)
 	move_timer.start()
+
+func add_body(coord: Vector2i) -> void:
+	current_occupied.insert(0,coord)
+	set_cell(coord, 0, Vector2i(0,0))
