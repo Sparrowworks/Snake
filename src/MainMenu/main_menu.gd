e7e67e3518adecb8771ca9dd32791568c3262a30
SP4R0W <sp4r0wxxx@gmail.com>
2025-01-03 12:24:18 +0100

    Finished help menu

--- src/MainMenu/main_menu.gd
+++ src/MainMenu/main_menu.gd
@@ -7,8 +7,8 @@ func _on_play_button_pressed() -> void:
 func _on_settings_button_pressed() -> void:
 	go_to("res://src/Settings/Settings.tscn")
 
-func _on_credits_button_pressed() -> void:
-	pass # Replace with function body.
+func _on_help_button_pressed() -> void:
+	go_to("res://src/Credits/Credits.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
@@ -17,7 +17,3 @@ func go_to(scene: String) -> void:
 	var transition: Node = Composer.setup_load_screen(Global.loading_transition)
 	await transition.finished_fade_in
 	Composer.load_scene(scene)
-
-
-func _on_help_button_pressed() -> void:
-	pass # Replace with function body.
